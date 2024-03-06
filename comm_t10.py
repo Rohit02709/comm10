0985f3e590c3745f50fb051fd00cf0eaaf59dbc4
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-03-06 16:04:22 +0100

    Update comm_t10.py

--- comm_t10.py
+++ comm_t10.py
@@ -269,10 +269,14 @@ with col10:
 
 if checkbox_value2:
     st.subheader('Own LSTM EUR/PLN D+5 prediction model')
-    val_D5E = pd.read_excel('LSTM_mv.xlsx', sheet_name='D+5_EUR')
-    val_D5EU = val_D5E[['Date','Day + 5 Prediction']]
-    fig_D5E = px.line(val_D5EU, x='Date', y=['Day + 5 Prediction'],color_discrete_map={'Day + 5 Prediction':'red'}, width=1000, height=500, 
-                  title=f'Day + 5 EUR/PLN prediction ') 
+    val_D5E = pd.read_excel('LSTM_mv.xlsx', sheet_name='D5_EUR')
+    val_D5EP = val_D5E[['Date','Day + 5 Prediction']]
+    val_D5EU = pd.read_excel('LSTM_mv.xlsx', sheet_name='D5_EUR')
+    val_D5EUR = val_D5EU[['Date','EUR/PLN']]
+    fig_D5E = px.line(val_D5EP, x='Date', y=['Day + 5 Prediction'],color_discrete_map={'Day + 5 Prediction':'red'}, width=1000, height=500, 
+                  title=f'Day + 5 EUR/PLN prediction ')
+    fig_D5E.add_trace(go.Scatter(x=val_D5EUR['Date'], y=val_D5EUR['EUR/PLN'], mode='lines', name='EUR/PLN', line=dict(color='blue')))
+
     fig_D5E.update_layout(plot_bgcolor='white',showlegend=True,xaxis=dict(showgrid=True, gridwidth=0.5, gridcolor='Lightgrey'),
                       yaxis=dict(showgrid=True, gridwidth=0.5, gridcolor='Lightgrey'))
     fig_D5E.add_vline(x = today,line_width=1, line_dash="dash", line_color="black")
